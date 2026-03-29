3dec72a42e3a3e23ed8f5445b4e5a2bb4364f2ca
Theodore Zurek-Dunne <99theodore@gmail.com>
2026-03-29 21:11:55 +0200

    refactor for omarchy...

--- git-add.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-git add ./hypr -f
-git add ./vicinae -f
-git add ./waybar -f
-git add ./systemd -f
-git add ./fastfetch -f
-git add ./ghostty -f
-git add ./.zshrc -f
--- waybar/scripts/bluetooth_status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-status=$(bluetoothctl show | grep "Powered:" | awk '{print $2}')
-if [ "$status" == "yes" ]; then
-    echo ""
-else
-    echo ""
-fi
